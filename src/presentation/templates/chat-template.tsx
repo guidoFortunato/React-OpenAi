5cb0820847fc05a3786d24098694c3b0b223d63d
guido fortunato <guido_fortunato@hotmail.com.ar>
2024-04-19 17:20:49 -0300

    sección 2 finalizada

--- src/presentation/templates/chat-template.tsx
+++ src/presentation/templates/chat-template.tsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import { GptMessage } from "../../components/chat-bubbles/GptMessage";
-import { MyMessage } from "../../components/chat-bubbles/MyMessage";
-import { TextMessageBox } from "../../components/chat-input-boxes/TextMessageBox";
-import { TypingLoader } from "../../components/loaders/TypingLoader";
-// import { TextMessageBoxFile } from "../../components/chat-input-boxes/TextMessageBoxFile";
-// import { TextMessageBoxSelect } from "../../components/chat-input-boxes/TextMessageBoxSelect";
+import { GptMessage } from "../components/chat-bubbles/GptMessage";
+import { MyMessage } from "../components/chat-bubbles/MyMessage";
+import { TextMessageBox } from "../components/chat-input-boxes/TextMessageBox";
+import { TypingLoader } from "../components/loaders/TypingLoader";
+
 
 interface Message {
   text: string;
   isGpt: boolean;
 }
 
-export const OrthographyPage = () => {
+export const ChatTemplate = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
 
@@ -21,7 +20,7 @@ export const OrthographyPage = () => {
 
     // Todo: USE CASE
     setIsLoading(false);
-
+    
     // Todo: añadir el mensaje de isGPT en true
   };
 
@@ -53,17 +52,6 @@ export const OrthographyPage = () => {
         placeholder="Escribe aquí lo que deseas"
         disableCorrections
       />
-      {/* <TextMessageBoxFile
-        onSendMessage={ handlePost }
-        placeholder="Escribe aquí lo que deseas"
-      /> */}
-      {/* <TextMessageBoxSelect
-        options={[
-          { id: "1", text: "Hola" },
-          { id: "2", text: "Mundo" },
-        ]}
-        onSendMessage={ console.log }
-      /> */}
     </div>
   );
 };
