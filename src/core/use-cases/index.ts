7560e8583d9f2dfa9deb729426cf184c1c0f6580
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-05-27 11:54:45 -0300

    text to audio

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from "./prons-cons.use-case";
 export * from "./prons-cons-stream.use-case";
 export * from "./prons-cons-stream-generator.use-case";
 export * from "./translate-text.use-case";
+export * from "./text-to-audio.use-case";
